33799d84e2bff13d28e4b137a15d6315d3f6d864
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2023-09-06 19:14:55 +0530

    correct the doubly problem

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -1,40 +1,61 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class node
 {
 public:
     int data;
-    node* next;
-    node* prev;
-    node(int val);
+    node *next;
+    node *prev;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
 };
 
-node::node(int val)
+void insertAtHead(node *&head, int val)
 {
-    node* n = new node(val);
-    next= NULL;
-    prev= NULL;
-}
-void insertAtHead(node* head, int val){
-    node* n = new node(val);
+    node *n = new node(val);
     n->next = head;
-    if (head!=NULL)
+    if (head != NULL)
     {
-        head->prev=n;
+        head->prev = n;
+        return;
     }
+    head=n;
 }
-void display(node* head){
-    node *temp = head;
-    while (temp!=NULL)
+void insertAtTail(node *&head, int val)
+{
+    if (head==NULL)
     {
-        cout<<temp->data<<"->";
-        temp=temp->next;
+        insertAtHead(head,val);
+        return;
     }
-    cout<<"NULL"<<endl;
+    
+    node *n = new node(val);
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+}
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
 }
 int main()
 {
-    node* head = NULL;
-    insertAtHead(head , 10 );
+    node *head = NULL;
+    insertAtHead(head, 10);
+    insertAtTail(head, 20);
     display(head);
 }
